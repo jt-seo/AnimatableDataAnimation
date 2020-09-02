006fc0bdc324784175e4005475bf860404b2217e
jt.seo <jt.seo@nexplayer.com>
2020-09-02 12:23:24 +0900

    Add test code to see how SwiftUI treats the animatableData.

--- AnimatableDataAnimation/ContentView.swift
+++ AnimatableDataAnimation/ContentView.swift
@@ -9,13 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var diamondWidth: CGFloat = 100
     var body: some View {
-        Text("Hello, World!")
+        Diamond(width: diamondWidth)
+            .onTapGesture {
+                withAnimation(.linear(duration: 2)) {
+                    self.diamondWidth = CGFloat.random(in: 100...300)
+                }
+            }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Diamond: Shape, Animatable {
+    var width: CGFloat
+    
+    var animatableData: CGFloat {
+        get {
+            print("get \(self.width)")
+            return self.width
+        }
+        set {
+            print("set \(self.width) -> \(newValue)")
+            self.width = newValue
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        print("path called. width: \(width)")
+        let point1 = CGPoint(x: rect.midX, y: rect.minY)
+        let point2 = CGPoint(x: rect.midX - width / 2, y: rect.midY)
+        let point3 = CGPoint(x: rect.midX, y: rect.maxY)
+        let point4 = CGPoint(x: rect.midX + width / 2, y: rect.midY)
+        
+        var p = Path()
+        p.move(to: point1)
+        p.addLine(to: point2)
+        p.addLine(to: point3)
+        p.addLine(to: point4)
+        
+        return p
     }
 }
